fb298f85e6b14bc988474a13ffe988ba6fd645bf
Chalivat <Chalivat.quentin@gmail.com>
2019-07-18 17:48:37 +0200

    player cool anim lol

--- SSRB/Assets/PlayerCanHit.cs
+++ SSRB/Assets/PlayerCanHit.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class PlayerCanHit : MonoBehaviour
 {
     public BoxCollider swordCollider;
+    public CharacterController controller;
+    public TrailRenderer ps;
+
+    void Start()
+    {
+        ps.emitting = false;
+    }
 
     public void canHit()
     {
         swordCollider.enabled = true;
+        ps.emitting = true;
     }
 
     public void cannontHit()
     {
         swordCollider.enabled = false;
+        ps.emitting = false;
+    }
+
+
+    public void canMove()
+    {
+        controller.enabled = true;
+    }
+
+    public void cannotMove()
+    {
+        controller.enabled = false;
     }
 }
